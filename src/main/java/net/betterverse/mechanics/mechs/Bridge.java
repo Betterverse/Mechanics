f4297fd5a389e2d7589c8b42c9e3e06828aac9e7
ZNickq <ZNickq@ZNickq-PC>
2012-06-26 16:23:23 +0300

    Implemented all mechanics!

--- src/main/java/net/betterverse/mechanics/mechs/Bridge.java
+++ src/main/java/net/betterverse/mechanics/mechs/Bridge.java
@@ -1,19 +1,21 @@
 package net.betterverse.mechanics.mechs;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import net.betterverse.mechanics.Mechanic;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.material.Sign;
 
+public class Bridge extends Mechanic {
 
-public class Bridge extends Mechanic{
 	private List<BlockFace> pos = new ArrayList<BlockFace>();
-	
+
 	public Bridge() {
 		super("Bridge");
 		pos.add(BlockFace.SOUTH);
@@ -37,14 +39,16 @@ public class Bridge extends Mechanic{
 		BlockFace toCheck = ((Sign) block.getState().getData()).getAttachedFace();
 		block = block.getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN);
 		block = block.getRelative(toCheck);
-		for(BlockFace posi : pos) {
-			if(posi == toCheck && posi.getOppositeFace() == toCheck) continue;
+		for (BlockFace posi : pos) {
+			if (posi == toCheck && posi.getOppositeFace() == toCheck) {
+				continue;
+			}
 			Set<Block> entirePart = getStart(block.getRelative(posi), posi);
-			for(Block st : entirePart) {
+			for (Block st : entirePart) {
 				switchStart(st, posi);
 			}
 		}
-		player.sendMessage(ChatColor.GREEN+"Switched bridge!");
+		player.sendMessage(ChatColor.GREEN + "Switched bridge!");
 	}
 
 	@Override
@@ -53,11 +57,29 @@ public class Bridge extends Mechanic{
 	}
 
 	private Set<Block> getStart(Block relative, BlockFace posi) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		Set<Block> toRet = new HashSet<Block>();
+		Material orig = relative.getType();
+		while (relative.getType() == orig) {
+			toRet.add(relative);
+			relative = relative.getRelative(posi);
+		}
+		return toRet;
 	}
 
 	private void switchStart(Block st, BlockFace posi) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		boolean create = st.getRelative(posi).getType() == Material.AIR;
+		Material toSet = st.getType();
+		st = st.getRelative(posi);
+		if (create) {
+			while (st.getType() == Material.AIR) {
+				st.setType(toSet);
+				st = st.getRelative(posi);
+			}
+		} else {
+			while (st.getType() == toSet) {
+				st.setType(Material.AIR);
+				st = st.getRelative(posi);
+			}
+		}
 	}
-
 }
--- src/main/java/net/betterverse/mechanics/mechs/Door.java
+++ src/main/java/net/betterverse/mechanics/mechs/Door.java
@@ -1,6 +1,84 @@
 package net.betterverse.mechanics.mechs;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import net.betterverse.mechanics.Mechanic;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.material.Sign;
 
-public class Door {
 
+public class Door extends Mechanic{
+
+	private List<BlockFace> pos = new ArrayList<BlockFace>();
+
+	public Door() {
+		super("Door");
+		pos.add(BlockFace.SOUTH);
+		pos.add(BlockFace.NORTH);
+		pos.add(BlockFace.WEST);
+		pos.add(BlockFace.EAST);
+	}
+
+	@Override
+	public boolean canCreate(Player player) {
+		return player.hasPermission("mechanics.door");
+	}
+
+	@Override
+	public boolean canUse(Player player) {
+		return true;
+	}
+
+	@Override
+	public void activate(Player player, Block block) {
+		BlockFace toCheck = ((Sign) block.getState().getData()).getAttachedFace();
+		Block against = block.getRelative(toCheck).getRelative(BlockFace.DOWN);
+		for (BlockFace posi : pos) {
+			if (posi == toCheck && posi.getOppositeFace() == toCheck) {
+				continue;
+			}
+			Set<Block> entirePart = getStart(against, posi);
+			for (Block st : entirePart) {
+				switchStart(st);
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Door";
+	}
+
+	private Set<Block> getStart(Block against, BlockFace posi) {
+		Set<Block> toRet = new HashSet<Block>();
+		Material orig = against.getType();
+		while (against.getType() == orig) {
+			toRet.add(against);
+			against = against.getRelative(posi);
+		}
+		return toRet;
+	}
+
+	private void switchStart(Block st) {
+		boolean create = st.getRelative(BlockFace.UP).getType() == Material.AIR;
+		Material orig = st.getType();
+		if(create) {
+			st = st.getRelative(BlockFace.UP);
+			while(st.getType() == Material.AIR) {
+				st.setType(orig);
+				st = st.getRelative(BlockFace.UP);
+			}
+		} else {
+			st = st.getRelative(BlockFace.UP);
+			while(st.getType() == orig) {
+				st.setType(Material.AIR);
+				st = st.getRelative(BlockFace.UP);
+			}
+		}
+	}
 }
